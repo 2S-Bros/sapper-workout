9a5727d0676c4996f7bd8269e51065fc45b9e838
EricSSartorius <EricSSartorius@gmail.com>
2019-09-14 16:58:14 -0700

    Add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<style lang="scss" scoped>
+  footer {
+    background: var(--dark-grey);
+    p {
+      color: var(--white);
+      text-align: center;
+    }
+  }
+</style>
+
+<footer>
+  <p>Epic Gains</p>
+</footer>
