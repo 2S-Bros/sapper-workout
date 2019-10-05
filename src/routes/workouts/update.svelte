80992ea1e667c87988c9fb4a1ca3f66246e38c36
EricSSartorius <EricSSartorius@gmail.com>
2019-10-05 12:22:52 -0700

    Form to update exercise and workout removed

--- src/routes/workouts/update.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-  <title>Update Workout</title>
-</svelte:head>
-
-<h1>Update an existing Workout</h1>
-<p>Form will go here to update workout</p>
-<h4>
-  <a href="workouts">Back to workouts</a>
-</h4>
